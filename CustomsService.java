cf24112774c7d403c0a14269141e298b6062d751
ksu-foxic <kseniamitina1985@gmail.com>
2025-06-03 23:13:48 +0300

    CustomsService.java

--- CustomsService.java
+++ CustomsService.java
@@ -1,9 +1,9 @@
 package ru.netology.service;
 
 public class CustomsService {
+    static final int WEIGH_DUTY = 100;
 
-    public static final int CalculateCustoms(int a, int b) {
-        final int WEIGH_DUTY = 100;
+    public static int CalculateCustoms(int a, int b) {
         int sum = a / 100 + b * WEIGH_DUTY;
         System.out.println("Размер пошлины (в руб.) составит: " + sum);
         return sum;
