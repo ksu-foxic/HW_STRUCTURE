9b5937e947f23c624a657bf48b922386e6267763
ksu-foxic <kseniamitina1985@gmail.com>
2025-06-03 23:12:28 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main {
         Scanner scanner = new Scanner((System.in));
         System.out.print("Введите цену товара (в руб.):");
         int price = scanner.nextInt();
-         System.out.print("Введите вес товара (в кг.): ");
+        System.out.print("Введите вес товара (в кг.): ");
         int weight = scanner.nextInt();
-        ru.netology.service.CustomsService.CalculateCustoms(price,weight);
+        ru.netology.service.CustomsService.CalculateCustoms(price, weight);
     }
- }
+}
